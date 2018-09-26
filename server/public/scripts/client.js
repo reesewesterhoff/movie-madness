cfacdf475f76b6dd137dcd1530706c6386d661fc
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-09-26 16:09:18 -0500

    add vendor js files, create app and controller

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,9 @@
 console.log('client js loaded');
+const movieApp = angular.module('MovieApp', []);
+
+movieApp.controller('MovieController', [function() {
+    let vm = this;
+    console.log('running MovieController');
+    vm.message = 'taco';
+
+}]);
