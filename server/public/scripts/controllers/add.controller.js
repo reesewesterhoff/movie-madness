57c360af121f17bd65696ff38c0d9349e4a58e50
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-09-27 10:40:44 -0500

    add moment.js and fix date and run time formats

--- server/public/scripts/controllers/add.controller.js
+++ server/public/scripts/controllers/add.controller.js
@@ -24,7 +24,11 @@ movieApp.controller('AddController', ['$http', function($http) {
             url: '/add'
         }).then(function(response) {
             console.log('back from server with movies', response.data);
-            vm.movieArray = response.data;
+            vm.movieArray = response.data.map(function(movie) {
+                movie.release_date = moment(movie.release_date).format("MMM Do YYYY"); 
+                movie.run_time = moment.duration(movie.run_time).hours() + ' hr ' + moment.duration(movie.run_time).minutes() + ' mins';
+                return movie;
+            });
         }).catch(function(error) {
             console.log('error getting movies from server', error);
         });
