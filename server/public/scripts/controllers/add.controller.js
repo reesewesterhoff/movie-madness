730a3beffcb037b14d52c74018782561fa3a82bc
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-09-28 13:22:31 -0500

    confirm popup on delete click, toast after deletion add view

--- server/public/scripts/controllers/add.controller.js
+++ server/public/scripts/controllers/add.controller.js
@@ -1,18 +1,19 @@
-movieApp.controller('AddController', ['$http', function($http) {
+movieApp.controller('AddController', ['$http', '$mdDialog', '$mdToast', function ($http, $mdDialog, $mdToast) {
     let vm = this;
     vm.movieToSend = {};
     vm.movieArray = [];
     vm.genreArray = [];
+    vm.status = '';
 
     vm.addMovie = function () {
         $http({
             method: 'POST',
             url: '/add',
             data: vm.movieToSend
-        }).then(function(response) {
+        }).then(function (response) {
             vm.movieToSend = {};
             vm.getMovies();
-        }).catch(function(error) {
+        }).catch(function (error) {
             console.log('error posting new movie to server', error);
         });
     }
@@ -21,26 +22,42 @@ movieApp.controller('AddController', ['$http', function($http) {
         $http({
             method: 'GET',
             url: '/add'
-        }).then(function(response) {
-            vm.movieArray = response.data.map(function(movie) {
-                movie.release_date = moment(movie.release_date).format("MMM Do YYYY"); 
+        }).then(function (response) {
+            vm.movieArray = response.data.map(function (movie) {
+                movie.release_date = moment(movie.release_date).format("MMM Do YYYY");
                 movie.run_time = moment.duration(movie.run_time).hours() + ' hr ' + moment.duration(movie.run_time).minutes() + ' mins';
                 return movie;
             });
-        }).catch(function(error) {
+        }).catch(function (error) {
             console.log('error getting movies from server', error);
         });
     }
 
     vm.deleteMovie = function (movieID) {
-        $http({
-            method: 'DELETE',
-            url: `/add/${movieID}`
-        }).then(function(response) {
-            vm.getMovies();
-        }).catch(function(error) {
-            console.log('error deleting movie from server', error);
+
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+            .title('Are you sure you want to delete this title?')
+            .textContent('You cannot undo this action')
+            .ariaLabel('Lucky day')
+            .targetEvent(movieID)
+            .ok('Fuck yeah!')
+            .cancel('Mommy I\'m scared');
+
+        $mdDialog.show(confirm).then(function () {
+            $http({
+                method: 'DELETE',
+                url: `/add/${movieID}`
+            }).then(function (response) {
+                vm.getMovies();
+            }).catch(function (error) {
+                console.log('error deleting movie from server', error);
+            });
+            $mdToast.show($mdToast.simple().textContent('Movie successfully deleted!'));
+        }, function () {
         });
+
+        
     }
 
     vm.getGenres = function () {
