88a611f57204c9cc0abf65eeb5159f64f7bb5e61
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-09-30 09:14:32 -0500

    api request working, displaying on DOM, working on displaying poster

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,13 +1,15 @@
 movieApp.controller('HomeController', ['$http', function($http) {
     let vm = this;
     console.log('running HomeController');
+    vm.movieArray = [];
 
     vm.getFromApi = function () {
         $http({
             method: 'GET',
-            url: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.descapi_key=0b68eb50758a2deff51a5daecc8c04fa`
+            url: `https://api.themoviedb.org/3/discover/movie?api_key=0b68eb50758a2deff51a5daecc8c04fa&`
         }).then(function(response) {
             console.log('from api', response);
+            vm.movieArray = response.data.results;
         }).catch(function(error) {
             console.log('error getting movies from api', error);
         });
